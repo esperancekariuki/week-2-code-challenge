817e8770c989daf2de9037d56c6f7a47282eeda9
esperancekariuki <kariukiesperance@gmail.com>
2022-12-05 10:59:49 +0300

    done challenge

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const section = document.queryselector('section')
+const section = document.querySelector('section')
 //get data from the json-server down below
 fetch('http://localhost:3000/characters')
-.then(function (res){
-    return res.json()
-})
-.then(function(res){
-    console.log(res)
-    console.log(res[4])
-    let output = ""
-    for(let i = 0; i<res.length; i++){
-        output += `<div class="singleAnimal">
-        <h2 class="animalName">${res[i].name}</h2>
-       
-    </div>`
+    .then((res) => res.json())
+    .then(function(res){
+        console.log(res)
+        console.log(res[4])
+        let output = ""
+        for(let i = 0; i<res.length; i++){
+            output += `<div class="singleAnimal">
+            <h2 class="animalName">${res[i].name}</h2>
+        
+        </div>`
 
     }
     section.innerHTML = output
@@ -29,7 +27,7 @@ fetch('http://localhost:3000/characters')
             const div = document.createElement("div")
             div.classList.add('description')
             
-            div.innerHTML = `<img src="${res[index].image}" alt="">
+            div.innerHTML = `<img src="${res[index].image}" alt--="">
             <p>votes: <span class="votesNumber">${res[index].votes}</span></p>
             <button type="button">add vote</button>`
             singleAnimal[index].append(div)
@@ -61,8 +59,3 @@ fetch('http://localhost:3000/characters')
     
     
 })
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
